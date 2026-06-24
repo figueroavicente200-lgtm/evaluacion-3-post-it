ed3dce5dcf13fa2437a88399986a48f6ca7d648f
figueroavicente200-lgtm <figueroavicente200@gmail.com>
2026-06-24 10:46:09 -0400

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 export function Note({ note, onDelete }) {
   return (
-    // Si la nota es importante, le añade la clase 'important' para pintarla roja
     <div className={`note ${note.important ? 'important' : ''}`}>
       <button className="delete-btn" onClick={() => onDelete(note.id)}>X</button>
       <h3>{note.title}</h3>
